a85e28ae78626be1a25dd0dd3b27734f0c8bfe2d
matth <cryptohackeur2.0@gmail.com>
2023-03-17 11:03:15 +0100

    Ajout de url dans la classe Task

--- app/src/main/java/com/example/todomobile/Task.java
+++ app/src/main/java/com/example/todomobile/Task.java
@@ -10,16 +10,18 @@ public class Task {
     private Date endDate;
     private String state;
     private String priority;
+    private String url;
 
-    public Task(String taskTitle, String description, Date beginDate, Date endDate, String state, String priority){
+    public Task(String taskTitle, String description, Date beginDate, Date endDate, String state, String priority, String url){
 
-        if(taskTitle!=null && description!=null && beginDate!=null && endDate!=null && state!=null && priority!=null){
+        if(taskTitle!=null && description!=null && beginDate!=null && endDate!=null && state!=null && priority!=null && url != null){
             this.taskTitle = taskTitle;
             this.description = description;
             this.beginDate = beginDate;
             this.endDate = endDate;
             this.state = state;
             this.priority = priority;
+            this.url = url;
         }
     }
 
@@ -29,6 +31,7 @@ public class Task {
     public Date getEndDate() {return endDate;}
     public String getState() {return state;}
     public String getPriority() {return priority;}
+    public String getUrl(){return url;}
 
     public void setTaskTitle(String taskTitle) {this.taskTitle = taskTitle;}
     public void setDescription(String description) {this.description = description;}
@@ -36,4 +39,5 @@ public class Task {
     public void setEndDate(Date endDate) {this.endDate = endDate;}
     public void setState(String state) {this.state = state;}
     public void setPriority(String priority) {this.priority = priority;}
+    public void setUrl(String url){this.url = url;}
 }
